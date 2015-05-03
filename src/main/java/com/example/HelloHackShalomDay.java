4809360c34a08a2ced1bc4d2c596353ccfdf8210
Marcel Sato <marcelsato@AleteiasMacAir.home>
2015-05-03 16:46:59 +0100

    Added Hello Shalom

---
+++ src/main/java/com/example/HelloHackShalomDay.java
@@ -0,0 +1,10 @@
+package main.java.com.example;
+
+/**
+ * Created by marcelsato on 5/3/15.
+ */
+public class HelloHackShalomDay {
+    public static void main(String[] args) {
+        System.out.println("Hello Shalom Hack Day!!!!");
+    }
+}
