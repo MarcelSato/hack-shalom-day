a50521c9b082e9defcd6b9b0f87f1e92b41a57dd
Marcel Sato <marcelsato@AleteiasMacAir.home>
2015-05-03 19:51:33 +0100

    Variant solution

--- src/main/java/com/example/HelloHackShalomDay.java
+++ src/main/java/com/example/HelloHackShalomDay.java
@@ -1,9 +1,6 @@
 package main.java.com.example;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Created by marcelsato on 5/3/15.
@@ -21,6 +18,8 @@ public class HelloHackShalomDay {
         numbers1.add(2);
         numbers1.add(1);
         numbers1.add(3);
+        numbers1.add(3);
+
 
         List<Integer> numbers2 = new ArrayList<>();
         numbers2.add(3);
@@ -34,25 +33,17 @@ public class HelloHackShalomDay {
     }
 
 
-    public static  showResult(List<Integer> numbers){
-        Set mySet = new HashSet<>();
-        for(int i = 0; i < numbers.size(); i++){
-                mySet.add(numbers.get(i));
+    public static List showResult(List<Integer> numbers){
+        List<Integer> d = new ArrayList<>();
+        for(int i  = 0; i < numbers.size(); i++){
+            for(int j = i + 1; j < numbers.size(); j++){
+                if(numbers.get(i) == numbers.get(j)){
+                    if(!d.contains(numbers.get(i))) d.add(numbers.get(i));
+                }
             }
-
-
-
-
-        return numbers.size() != mySet.size();
-
-
-
-
-
-//            if(numbers.size() > mySet.size()){
-//                return true;
-//            }
-//            return false;
+        }
+        Collections.sort(d);
+        return d;
     }
 
 
