3e48e3c942949a779d7fea0c6d59d6b2fb1d93d4
MarcelSato <MarcelSato@users.noreply.github.com>
2015-05-05 16:24:00 +0100

    Changes

--- src/main/java/com/example/HelloHackShalomDay.java
+++ src/main/java/com/example/HelloHackShalomDay.java
@@ -62,7 +62,7 @@ public class HelloHackShalomDay {
         System.out.println("List 3" + showResult(numbers3) + "\n");
         System.out.println("List 4" + showResult(numbers4) + "\n");
         System.out.println("List 5" + showResult(numbers5) + "\n");
-
+    // Comment to test repository
     }
 
 
